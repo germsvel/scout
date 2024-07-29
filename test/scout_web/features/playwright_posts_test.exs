70ff4b24fc29a66c832ee167d2828210d960eab5
German Velasco <germsvel@gmail.com>
2024-07-29 08:53:01 -0400

    wip: adding forms

--- test/scout_web/features/playwright_posts_test.exs
+++ test/scout_web/features/playwright_posts_test.exs
@@ -22,5 +22,20 @@ defmodule ScoutWeb.PlaywrightPostsTests do
       assert Page.text_content(page, "#posts-#{post1.id}") =~ post1.body
       assert Page.text_content(page, "#posts-#{post2.id}") =~ post2.body
     end
+
+    @create_attrs %{body: "some body", draft: true, published_date: "2024-02-17"}
+
+    test "user can save new post", %{page: page} do
+      url = ScoutWeb.Endpoint.url() <> "/posts"
+
+      page |> Page.goto(url)
+
+      Page.click(page, "a", %{text: "New Post"})
+
+      # |> click_link("New Post")
+      # |> fill_in("Body", with: @invalid_attrs.body)
+      # |> fill_in("Published date", with: @invalid_attrs.published_date)
+      # |> assert_has("#post-form", text: "can't be blank")
+    end
   end
 end

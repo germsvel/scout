d779fb59dcec959cc56b60d91d74d74d5478834a
German Velasco <germsvel@gmail.com>
2024-08-14 15:37:02 -0400

    Rename index_live and show_live to view

--- test/scout_web/live/post_live_test.exs
+++ test/scout_web/live/post_live_test.exs
@@ -33,84 +33,84 @@ defmodule ScoutWeb.PostLiveTest do
     end
 
     test "lists all posts", %{conn: conn, post: post} do
-      {:ok, _index_live, html} = live(conn, ~p"/posts")
+      {:ok, _view, html} = live(conn, ~p"/posts")
 
       assert html =~ "Listing Posts"
       assert html =~ post.body
     end
 
     test "saves new post", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, ~p"/posts")
+      {:ok, view, _html} = live(conn, ~p"/posts")
 
-      assert index_live |> element("a", "New Post") |> render_click() =~
+      assert view |> element("a", "New Post") |> render_click() =~
                "New Post"
 
-      assert_patch(index_live, ~p"/posts/new")
+      assert_patch(view, ~p"/posts/new")
 
-      assert index_live
+      assert view
              |> form("#post-form", post: @invalid_attrs)
              |> render_change() =~ "can&#39;t be blank"
 
-      assert index_live
+      assert view
              |> form("#post-form", post: @create_attrs)
              |> render_submit()
 
-      assert_patch(index_live, ~p"/posts")
+      assert_patch(view, ~p"/posts")
 
-      html = render(index_live)
+      html = render(view)
       assert html =~ "Post created successfully"
       assert html =~ "some body"
     end
 
     test "saves new post with has_element?", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, ~p"/posts")
+      {:ok, view, _html} = live(conn, ~p"/posts")
 
-      assert index_live |> element("a", "New Post") |> render_click() =~
+      assert view |> element("a", "New Post") |> render_click() =~
                "New Post"
 
-      assert_patch(index_live, ~p"/posts/new")
+      assert_patch(view, ~p"/posts/new")
 
-      assert index_live
+      assert view
              |> form("#post-form", post: @invalid_attrs)
              |> render_change() =~ "can&#39;t be blank"
 
-      assert index_live
+      assert view
              |> form("#post-form", post: @create_attrs)
              |> render_submit()
 
-      assert_patch(index_live, ~p"/posts")
+      assert_patch(view, ~p"/posts")
 
-      assert has_element?(index_live, "[role=alert]", "Post created successfully")
+      assert has_element?(view, "[role=alert]", "Post created successfully")
     end
 
     test "updates post in listing", %{conn: conn, post: post} do
-      {:ok, index_live, _html} = live(conn, ~p"/posts")
+      {:ok, view, _html} = live(conn, ~p"/posts")
 
-      assert index_live |> element("#posts-#{post.id} a", "Edit") |> render_click() =~
+      assert view |> element("#posts-#{post.id} a", "Edit") |> render_click() =~
                "Edit Post"
 
-      assert_patch(index_live, ~p"/posts/#{post}/edit")
+      assert_patch(view, ~p"/posts/#{post}/edit")
 
-      assert index_live
+      assert view
              |> form("#post-form", post: @invalid_attrs)
              |> render_change() =~ "can&#39;t be blank"
 
-      assert index_live
+      assert view
              |> form("#post-form", post: @update_attrs)
              |> render_submit()
 
-      assert_patch(index_live, ~p"/posts")
+      assert_patch(view, ~p"/posts")
 
-      html = render(index_live)
+      html = render(view)
       assert html =~ "Post updated successfully"
       assert html =~ "some updated body"
     end
 
     test "deletes post in listing", %{conn: conn, post: post} do
-      {:ok, index_live, _html} = live(conn, ~p"/posts")
+      {:ok, view, _html} = live(conn, ~p"/posts")
 
-      assert index_live |> element("#posts-#{post.id} a", "Delete") |> render_click()
-      refute has_element?(index_live, "#posts-#{post.id}")
+      assert view |> element("#posts-#{post.id} a", "Delete") |> render_click()
+      refute has_element?(view, "#posts-#{post.id}")
     end
   end
 
@@ -118,31 +118,31 @@ defmodule ScoutWeb.PostLiveTest do
     setup [:create_post]
 
     test "displays post", %{conn: conn, post: post} do
-      {:ok, _show_live, html} = live(conn, ~p"/posts/#{post}")
+      {:ok, _view, html} = live(conn, ~p"/posts/#{post}")
 
       assert html =~ "Show Post"
       assert html =~ post.body
     end
 
     test "updates post within modal", %{conn: conn, post: post} do
-      {:ok, show_live, _html} = live(conn, ~p"/posts/#{post}")
+      {:ok, view, _html} = live(conn, ~p"/posts/#{post}")
 
-      assert show_live |> element("a", "Edit") |> render_click() =~
+      assert view |> element("a", "Edit") |> render_click() =~
                "Edit Post"
 
-      assert_patch(show_live, ~p"/posts/#{post}/show/edit")
+      assert_patch(view, ~p"/posts/#{post}/show/edit")
 
-      assert show_live
+      assert view
              |> form("#post-form", post: @invalid_attrs)
              |> render_change() =~ "can&#39;t be blank"
 
-      assert show_live
+      assert view
              |> form("#post-form", post: @update_attrs)
              |> render_submit()
 
-      assert_patch(show_live, ~p"/posts/#{post}")
+      assert_patch(view, ~p"/posts/#{post}")
 
-      html = render(show_live)
+      html = render(view)
       assert html =~ "Post updated successfully"
       assert html =~ "some updated body"
     end

ed040ce9956d792fe46b6decc15d59c8593d33ba
German Velasco <germsvel@gmail.com>
2024-08-24 12:25:16 -0400

    Add 100 times tests for PhoenixTest for dead views

--- test/scout_web/features/account_management_test.exs
+++ test/scout_web/features/account_management_test.exs
@@ -27,6 +27,22 @@ defmodule ScoutWeb.AccountManagementTest do
       |> assert_has("#users", text: create_attrs.name)
     end
 
+    for i <- 1..100 do
+      test "can create new user #{i}", %{conn: conn} do
+        create_attrs = %{name: "some name", age: 42, active_at: "2024-02-16 15:08:00"}
+
+        conn
+        |> visit(~p"/users")
+        |> click_link("New User")
+        |> fill_in("Name", with: create_attrs.name)
+        |> fill_in("Age", with: create_attrs.age)
+        |> fill_in("Active at", with: create_attrs.active_at)
+        |> click_button("Save User")
+        |> assert_has("#flash-group", text: "User created successfully")
+        |> assert_has("#users", text: create_attrs.name)
+      end
+    end
+
     test "renders errors when data is invalid", %{conn: conn} do
       conn
       |> visit(~p"/users")

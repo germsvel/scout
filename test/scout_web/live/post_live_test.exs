cf5c1e14d71aef518f96ae9a8e1c86d438fad9a5
German Velasco <germsvel@gmail.com>
2024-08-26 08:46:57 -0400

    Arrange PostLiveTest in pain points section

--- test/scout_web/live/post_live_test.exs
+++ test/scout_web/live/post_live_test.exs
@@ -13,28 +13,8 @@ defmodule ScoutWeb.PostLiveTest do
     %{post: post}
   end
 
-  describe "Index" do
+  describe "Pain points" do
     setup [:create_post]
-
-    test "user can navigate to their own posts", %{conn: conn} do
-      {:ok, view, _html} = live(conn, ~p"/posts")
-
-      {:ok, conn} =
-        view
-        |> element("a", "Users")
-        |> render_click()
-        |> follow_redirect(conn)
-
-      html = html_response(conn, 200)
-      assert html =~ "Users"
-    end
-
-    test "lists all posts", %{conn: conn, post: post} do
-      {:ok, _view, html} = live(conn, ~p"/posts")
-
-      assert html =~ "Listing Posts"
-      assert html =~ post.body
-    end
 
     test "user can create new post", %{conn: conn} do
       {:ok, view, _html} = live(conn, ~p"/posts/new")
@@ -52,6 +32,70 @@ defmodule ScoutWeb.PostLiveTest do
       html = render(view)
       assert html =~ "Post created successfully"
       assert html =~ "some body"
+    end
+
+    test "saves new post with has_element?", %{conn: conn} do
+      {:ok, view, _html} = live(conn, ~p"/posts/new")
+
+      view
+      |> form("#post-form",
+        post: %{
+          body: "some body",
+          draft: true,
+          published_date: "2024-02-17"
+        }
+      )
+      |> render_submit()
+
+      assert has_element?(view, "[role=alert]", "Post created successfully")
+      assert has_element?(view, "#posts", "some body")
+    end
+
+    test "simple navigation + post creation", %{conn: conn} do
+      {:ok, view, _html} = live(conn, ~p"/posts")
+
+      assert view |> element("a", "New Post") |> render_click() =~
+               "New Post"
+
+      assert_patch(view, ~p"/posts/new")
+
+      assert view
+             |> form("#post-form", post: @invalid_attrs)
+             |> render_change() =~ "can&#39;t be blank"
+
+      assert view
+             |> form("#post-form", post: @create_attrs)
+             |> render_submit()
+
+      assert_patch(view, ~p"/posts")
+
+      html = render(view)
+      assert html =~ "Post created successfully"
+      assert html =~ "some body"
+    end
+
+    test "user can navigate to their own posts", %{conn: conn} do
+      {:ok, view, _html} = live(conn, ~p"/posts")
+
+      {:ok, conn} =
+        view
+        |> element("a", "Users")
+        |> render_click()
+        |> follow_redirect(conn)
+
+      html = html_response(conn, 200)
+      assert html =~ "Users"
+    end
+  end
+
+  describe "Index" do
+    setup [:create_post]
+
+    test "lists all posts", %{conn: conn, post: post} do
+      {:ok, _view, html} = live(conn, ~p"/posts")
+
+      assert html =~ "Listing Posts"
+      assert html =~ post.body
     end
 
     test "saves new post", %{conn: conn} do
@@ -75,27 +119,6 @@ defmodule ScoutWeb.PostLiveTest do
       html = render(view)
       assert html =~ "Post created successfully"
       assert html =~ "some body"
-    end
-
-    test "saves new post with has_element?", %{conn: conn} do
-      {:ok, view, _html} = live(conn, ~p"/posts")
-
-      assert view |> element("a", "New Post") |> render_click() =~
-               "New Post"
-
-      assert_patch(view, ~p"/posts/new")
-
-      assert view
-             |> form("#post-form", post: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      assert view
-             |> form("#post-form", post: @create_attrs)
-             |> render_submit()
-
-      assert_patch(view, ~p"/posts")
-
-      assert has_element?(view, "[role=alert]", "Post created successfully")
     end
 
     test "updates post in listing", %{conn: conn, post: post} do
